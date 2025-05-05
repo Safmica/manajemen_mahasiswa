c071e55bf0c45c45bbf5d34378c255dd820bd4a1
Safmica <safmikasa@gmail.com>
2025-05-05 14:17:38 +0700

    fix: laravel api rputes 404

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\JurusansController;
 use App\Http\Controllers\ProdisController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/fakultas', [FakultasController::class, 'getFakultas']);
 Route::get('/jurusan', [JurusansController::class, 'getJurusan']);
